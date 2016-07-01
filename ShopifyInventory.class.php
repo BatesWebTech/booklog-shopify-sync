a1c7c8f980ffe7180d97a7b80b773ce2f03dd21a
Jake Paris <jparis@bates.edu>
2016-07-01 17:08:15 -0400

    Added the ability to upload the csv and parse it. Added some style.

--- ShopifyInventory.class.php
+++ ShopifyInventory.class.php
@@ -3,38 +3,120 @@
 class ShopifyInventory extends Shopify {
 
 	var $updatesToRun = [];
+	var $quantityUpdates;
+
+	var $counts = array(
+		'matched' => 0,
+		'errored' => 0,
+		'csv_rows' => 0,
+		'updated' => 0
+	);
+
+	function count($which,$num=false) {
+		if($num)
+			$this->counts[$which] = $this->counts[$which] + $num;
+		else
+			return $this->counts[$which];
+	}
+
+	function countMatched($num=false) {
+		return $this->count('matched',$num);
+	}
+	function countErrored($num=false) {
+		return $this->count('errored',$num);
+	}
+	function countUpdated($num=false) {
+		return $this->count('updated',$num);
+	}
+	function countUnmatched() {
+		return ($this->countCsvRows() - $this->countMatched() );
+	}
+	function countCsvRows($num=false) {
+		return $this->count('csv_rows',$num);
+	}
 
 	function setQuantityUpdates($vals) {
 		$this->quantityUpdates = $vals;
 	}
 
 	function doBatchVariantUpdates(){
+		foreach($this->updatesToRun as $vid => $newData){
 
-		foreach($this->updatesToRun as $vid => $newQuantity){
-			$res = $this->updateVariant($vid,array('inventory_quantity'=>$newQuantity));
-			if( property_exists($res,'errors'))
-				echo '<p style="color:red">Error: '. $res->errors .'</p>';
-			else
-				echo '<p>Updated barcode <b>'.$res->variant->barcode.'</b> to quantity <b>'.$res->variant->inventory_quantity . '</b></p>';
+			usleep(500000); // rate limit to .5 seconds so we dont' hit our api limit
+			$res = $this->updateVariant($vid,$newData);
+			if( property_exists($res,'errors')) {
+
+				echo '<li style="color:red">Error: '. json_encode($res->errors) .'</li>';
+				$this->countErrored(1);
+			} else {
+				$this->countUpdated(1);
+				echo '<li>Updated barcode <b>'.$res->variant->barcode.'</b> to quantity <b>'.$res->variant->inventory_quantity . '</b></li>';
+			}
 		}
 		$this->updatesToRun = [];
 	}
 
 	function updateInventory() {
 		$products = $this->getAllProducts();
-
+		echo '<ol>';
 		foreach($products as $product) {
 			foreach($product->variants as $variant){
 				if( array_key_exists($variant->barcode,$this->quantityUpdates) ) {
+	
+					$this->countMatched(1);
 
-					$this->updatesToRun[ $variant->id ] = $this->quantityUpdates[ $variant->barcode ];
+					$quantity = $this->quantityUpdates[ $variant->barcode ];
+					$oldQuantity = $variant->inventory_quantity;
+
+					// if Shopify is set not to track inventory, skip this one
+					if( $variant->inventory_management != 'shopify' )
+						continue;
+
+					// only need to update if the quantity is different.
+					if( $quantity == $oldQuantity)
+						continue;
+
+					$this->updatesToRun[ $variant->id ] = array(
+						'inventory_quantity' => $quantity,
+						'old_inventory_quantity' => $oldQuantity
+					);
+
+					// run the batch 20 at a time. 
 					if( count($this->updatesToRun) > 20 )
 						$this->doBatchVariantUpdates();
 
+				} else {
+					
 				}
 			}
 		}
 		$this->doBatchVariantUpdates();
+		echo '</ol>';
+		$this->countUnmatched();
+	}
+
+	function parseCSV($filename,$inventoryHeader,$barcodeHeader) {
+		$csv = array_map('str_getcsv', file( $filename ));
+		$headerRow = array_shift($csv);
+		$processedCsv = [];
+		foreach($csv as $rowKey => $row){
+			foreach($row as $k=>$v){
+				$processedCsv[$rowKey][ $headerRow[$k] ] = $v;
+			}
+		}
+		unset($csv);
+		$this->countCsvRows( count($processedCsv) );
+		// $inventoryKey = array_search($inventoryHeader, $headerRow );
+		// $barcodeKey = array_search($barcodeHeader, $headerRow );
+		$toUpdate = [];
+		// echo'<pre>';var_export($processedCsv);echo'</pre>';die;
+		foreach($processedCsv as $row) {
+			$barcode = $row[$barcodeHeader];
+			$inventoryCount = $row[$inventoryHeader];
+
+			$toUpdate[ $barcode ] = $inventoryCount;
+		}
+		$this->setQuantityUpdates( $toUpdate );
 	}
 
 }
--- index.php
+++ index.php
@@ -1,22 +1,92 @@
 <?php
-ini_set('display_errors', '1');
+// ini_set('display_errors','1');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bates Inventory Updater</title>
+	<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
 
-require 'Shopify.class.php';
-require 'ShopifyInventory.class.php';
+<?php
+// ini_set('display_errors', '1');
+
+$loginform = <<<FORM
+<form action="" method="post" class="login">
+	Enter the password: <input type="text" value="" name="cookiepwd">
+	<input type="submit" name="submit-cookie" value="login">
+	</form>
+FORM;
+// Just some temporary easy security
+if( isset($_POST['cookiepwd']) ) {
+	sleep(2);
+	if( $_POST['cookiepwd'] == 'goodness' ){
+		setcookie('nosecurity','this is just temporary');
+		// flow through
+	} else {
+		echo '<p class="error">Wrong</p>';
+		echo $loginform;
+		die;
+	}
+} else if( $_COOKIE['nosecurity'] !== 'this is just temporary' ) {
+	echo $loginform;
+	die;
+}
 
 
-define('STORE_NAME','bates-college-store-dev');
-define('API_KEY','APIKEYGOESHERE');
-define('API_SECRET','APIKEYGOESHERE');
-define('API_PASSWORD','APIKEYGOESHERE');
 
-$s = new ShopifyInventory(STORE_NAME,API_KEY,API_PASSWORD);
+if( isset($_POST['submit'])) :
 
-/* This is in format
-	barcode => new quantity
- */
-$s->setQuantityUpdates(array(
-	'684418864012' => '2'
-));
+	if( $_FILES['csv']['tmp_name'] != '' ) {
 
-$s->updateInventory();
+
+	echo '<div class="ouput">';
+	// Setup Shopify Connection
+	require 'Shopify.class.php';
+	require 'ShopifyInventory.class.php';
+
+	define('STORE_NAME','bates-college-store-dev');
+	define('API_KEY','APIKEYGOESHERE');
+	define('API_SECRET','APIKEYGOESHERE');
+	define('API_PASSWORD','APIKEYGOESHERE');
+	$s = new ShopifyInventory(STORE_NAME,API_KEY,API_PASSWORD);
+
+
+	// Parse CSV
+	/* This is in format
+		barcode => new quantity
+	 */
+	$inventoryCSVHeader = 'in_qty_onhand';
+	$barcodeCSVHeader = 'in_isbn';
+	$s->parseCSV( $_FILES['csv']['tmp_name'], $inventoryCSVHeader, $barcodeCSVHeader );
+	$s->updateInventory();
+
+	echo '</div>';
+
+	?> 
+	
+	<div class="finish-message">
+		<p>Matched <b><?php echo $s->countMatched() ?></b> out of <b><?php echo $s->countCsvRows() ?></b> barcodes.</p>
+		<p class="success">Updated <?php echo $s->countUpdated() ?> product variants</p>
+		<p class="warning">No changes to <?php echo ($s->countMatched() - $s->countUpdated()) ?> product variants.</p>
+		<p class="error">Errored <?php echo $s->countErrored() ?> times.</p>
+	</div>
+
+<?php
+	}
+endif; // end posted
+
+
+?>
+
+<form method="post" action="" enctype="multipart/form-data" class="upload-csv">
+
+<h2>Upload a csv file</h2>
+<input type="file" name="csv" value=""><input type="submit" value="Run Update" name="submit">
+
+</form>
+
+
+</body>
+</html>
