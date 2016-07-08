d1552a81e0a8f7d931518f9cb4d2495a1c2f2072
Jake Paris <jparis@bates.edu>
2016-07-08 13:33:37 -0400

    put config into a config-dist file

---
+++ config-dist.php
@@ -0,0 +1,26 @@
+<?php
+
+define('STORE_NAME',$_GET['shop']);
+
+// get this stuff @https://app.shopify.com/services/partners/api_clients/
+
+// PROD app
+// define('API_KEY','APIKEYGOESHERE');
+// define('API_SECRET','secretkeyhash');
+
+// DEV app
+// define('API_KEY','APIKEYGOESHERE');
+// define('API_SECRET','secretkeyhash');
+
+
+// DEV
+// define('DB_USER','shopify');
+// define('DB_NAME','shopify');
+// define('DB_PASS','dbpassword');
+// define('DB_HOST', 'dbpassword');
+
+// PROD
+// define('DB_USER','shopify');
+// define('DB_NAME','shopify');
+// define('DB_PASS','dbpassword');
+// define('DB_HOST', 'bates.edu');
