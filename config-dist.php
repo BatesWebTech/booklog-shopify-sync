f85ff66662456eb415215c40d2d2c869775631b2
Jake Paris <jparis@bates.edu>
2017-01-12 10:32:24 -0500

    update config

--- config-dist.php
+++ config-dist.php
@@ -1,26 +1,12 @@
 <?php
 
 define('STORE_NAME',$_GET['shop']);
-
 // get this stuff @https://app.shopify.com/services/partners/api_clients/
-
-// PROD app
-// define('API_KEY','APIKEYGOESHERE');
-// define('API_SECRET','secretkeyhash');
-
-// DEV app
-// define('API_KEY','APIKEYGOESHERE');
-// define('API_SECRET','secretkeyhash');
+// define('API_KEY','');
+// define('API_SECRET','');
 
 
-// DEV
-// define('DB_USER','shopify');
-// define('DB_NAME','shopify');
-// define('DB_PASS','dbpassword');
-// define('DB_HOST', 'dbpassword');
-
-// PROD
-// define('DB_USER','shopify');
-// define('DB_NAME','shopify');
-// define('DB_PASS','dbpassword');
-// define('DB_HOST', 'bates.edu');
+// define('DB_USER','');
+// define('DB_NAME','');
+// define('DB_PASS','');
+// define('DB_HOST', '');
